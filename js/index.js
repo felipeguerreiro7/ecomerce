697da618efd5b040dd6deeefe0f1724269540b60
Felipeguerreiro7 <lf.guerreiroalves@gmail.com>
2022-01-04 03:27:10 -0300

    Arrumando os cards

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ startStore = () => {
     let containerProdutos = document.getElementById('produtos');
     items.map((val) => {
         containerProdutos.innerHTML += `       
-        <div class="produto-single">
-            <img src"`+val.img+`"/>
-            <p>`+val.nome+`</p>
-            <p>`+val.preco+`</p>
-            <a key="`+val.id+`" href="#">Adicionar ao Carrinho!</a>
+        <div class="produto-single card">
+            <img src="`+val.img+`" class="card-img-top"/>
+            <div class="card-body">
+            <p class="card-title">`+val.nome+`</p>
+            <p class="card-text">`+val.preco+`</p>
+            <a key="`+val.id+`" href="#" class="btn btn-primary btn-sm">Adicionar ao Carrinho!</a>
+            </div>
         </div>
         `
     })
